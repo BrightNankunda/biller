4b84421a8bee640466d3a0d4eb9237dae1343c95
BrightNankunda <67958069+BrightNankunda@users.noreply.github.com>
2021-06-03 17:12:21 +0300

    Fetching bills commit

--- src/Components/BillComponents/AllBills.js
+++ src/Components/BillComponents/AllBills.js
@@ -1,6 +1,5 @@
 import React, {useEffect} from 'react'
-import {useDispatch} from 'react-redux'
-import {useSelector} from 'redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {AllBills as GetAllBills} from '../../Actions/BillActions'
 
 const AllBills = () => {
@@ -13,13 +12,27 @@ const AllBills = () => {
       }
    }, [])
 
-   // const {bills} = useSelector(state => state.bills)
-   // const {loading, bills: AllBills, error} = bills
-   // console.log(bills)
-
+   const AllSavedBills = useSelector(state => state.bills)
+   const {loading, bills, error} = AllSavedBills
+   
+   const capitalize = (word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1)
+   }
    return ( 
       <div className="w-100">
          <h4 className="text-center">ALL BILLS</h4>
+         {loading && <h2 className="text-center">Loading...</h2>}
+         {bills && bills.map(bill => 
+            <div className="list-group">
+               <h5 className="list-group-item text-center">{capitalize(bill.propertyType)}</h5>
+               <h5 className="list-group-item text-center">{bill.advocate}</h5>
+               <h5 className="list-group-item text-center text-primary">
+               Total: Ugx {bill.total}</h5>
+            </div>
+         )}
+         {error && <div className="alert alert-danger">
+            {error}
+         </div>}
       </div>
    );
 }
